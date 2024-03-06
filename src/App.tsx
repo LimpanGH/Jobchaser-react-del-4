6c4d2ea2f9e786e9d71553ffe83af2e8886bedc7
LimpanGH <Linusj555@gmail.com>
2024-03-06 16:31:58 +0100

    jic

--- src/App.tsx
+++ src/App.tsx
@@ -32,4 +32,128 @@ import './App.css'
 //   )
 // }
 
+
+import './App.css';
+
+//Objektdata
+const data = [
+  {
+    name: 'Sandra Larsson',
+    skill: 'FrontendDeveloper',
+    city: 'Stockholm',
+    picture: 'https://randomuser.me/api/portraits/med/women/35.jpg',
+    born: '1980',
+  },
+  {
+    name: 'Karl Karlsson',
+    skill: 'BackendDeveloper',
+    city: 'Göteborg',
+    picture: 'https://randomuser.me/api/portraits/med/men/35.jpg',
+    born: '1985',
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        {data.map((person) => {
+          return (
+            <ProfileCard
+              name={person.name}
+              skills={person.skill}
+              city={person.city}
+              picture={person.picture}
+              born={person.born}
+            />
+          );
+        })}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+/*function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+          <ProfileCard
+            // Props - information till kompoenten - som argument till funktionen!
+            name="Sandra"
+            skills="JS React"
+            city="Stockholm"
+            born={1980}
+          />  
+      </main>
+      <Footer />
+    </div>
+  );
+}*/
+
+function Header() {
+  return (
+    <header>
+      <h1>Developer</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>@copyrighttjossan</p>
+    </footer>
+  );
+}
+
+// Med destrucering av props
+function ProfileCard({ name, skills, city, born, picture }) {
+  // Logic (JS) goes here or inside {}
+  // Här är det ok med if, for osv,
+  const yearNow = new Date().getFullYear();
+  /* if (props) {
+    console.log(name, skills, city, born);
+  }*/
+
+  return (
+    // Här inom {} kan du inte använda for, if, switch, men ternary och logiska operatorer som && mfl
+    // Attribut heter lite annorlunda ex class => className
+    <article className="card" style={{ borderRadius: '20px' }}>
+      <h2>{name}</h2>
+      <p>Skills: {skills}</p>
+      <p>{city}</p>
+      <img src={picture} />
+      <p>Age: {yearNow - born}</p>
+    </article>
+  );
+}
+/*
+function ProfileCard(props) {
+  // Logic (JS) goes here or inside {}
+  // Här är det ok med if, for osv,
+  const yearNow = new Date().getFullYear();
+  if (props) {
+    console.log(props.name, props.skills, props.city, props.age);
+  }
+
+
+  return (
+    // Här inom {} kan du inte använda for, if, switch, men ternary och logiska operatorer som && mfl
+    // Attribut heter lite annorlunda ex class => className
+    <article className="card" style={{ borderRadius: '20px' }}>
+      <h2>{props.name}</h2>
+      <p>Skills: {props.skills}</p>
+      <p>{props.city}</p>
+      <img />
+      <p>Age: {yearNow - props.born}</p>
+    </article>
+  );
+}*/
+
+export default App;
+
+
 export default App
