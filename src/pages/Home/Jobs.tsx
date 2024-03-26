cdcae5c40a37c4869a3c1e52b4c2fef62b311db8
LimpanGH <Linusj555@gmail.com>
2024-03-26 15:07:07 +0100

    changed path to fallback-icon

--- src/pages/Home/Jobs.tsx
+++ src/pages/Home/Jobs.tsx
@@ -131,7 +131,9 @@ export function Jobs(): JSX.Element {
       <div className='logo'>
         <img
           className='logo-image'
-          src={job.logo_url ? job.logo_url : './src/images/JobChaser-SVG.svg '}
+          src={
+            job.logo_url ? job.logo_url : './public/images/JobChaser-SVG.svg '
+          }
           alt=''
         />
       </div>
