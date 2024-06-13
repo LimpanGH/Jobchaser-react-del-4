f79d869355246e980eb7000af7f3bb6dda2d065f
LimpanGH <Linusj555@gmail.com>
2024-06-13 15:32:16 +0200

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Set this to the base URL you need
+  base: '/Jobchaser/', // Set this to the base URL you need
   plugins: [react()],
  
 });
