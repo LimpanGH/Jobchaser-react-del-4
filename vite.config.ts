89e43f6db475e6e4ab76495cff53299a6ecb6c62
LimpanGH <Linusj555@gmail.com>
2024-06-13 15:09:44 +0200

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/', // Set this to the base URL you need
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        // Ensure JavaScript files are treated as ES modules
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
-      }
-    }
-  }
+ 
 });
